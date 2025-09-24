bd872ab59ce695b76e633abeb434e41d369c4b00
Reihane2783 <reihane2783@gmail.com>
2025-09-25 01:50:57 +0330

    Update q1.c

--- q1.c
+++ q1.c
@@ -3,22 +3,33 @@
 #include <math.h>
 
 int main()
-{float x;
-double summand=1,sourat,makhraj,s=0,total=0;
-int i=0,sign=1;
-  printf("Enter x as a float number and 0.0<x<1.0: \n");
-  scanf("%f",&x);
-  if (x>0 && x<1)
-    {while(summand>1E-4)
-     {i++;
-     sourat=pow(x,2*(i-1));
-     makhraj=sign *(pow(x,i)/i);
-     total+=makhraj;
-     summand= sourat/total;
-     s += sign * summand;
-     sign= -sign;}
+{
+    float x;
+    double summand = 1, numerator, denominator, s = 0, total = 0;
+    int i = 0, sign = 1;
 
-   printf("S=%f\n",s);}
-  else
-  {printf("invalid\n");}
-    return 0;}
+    printf("Enter x as a float number such that 0.0 < x < 1.0:\n");
+    scanf("%f", &x);
+
+    if (x > 0 && x < 1)
+    {
+        while (summand > 1E-4)
+        {
+            i++;
+            numerator = pow(x, 2 * (i - 1));
+            denominator = sign * (pow(x, i) / i);
+            total += denominator;
+            summand = numerator / total;
+            s += sign * summand;
+            sign = -sign;
+        }
+
+        printf("S = %f\n", s);
+    }
+    else
+    {
+        printf("Invalid input. Please enter a value between 0 and 1.\n");
+    }
+
+    return 0;
+}
